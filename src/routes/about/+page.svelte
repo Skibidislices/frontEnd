99bf6d74e47914fe3296d0dfac8095ff53865a19
Ivan Iliev <ilie0012@hz.nl>
2024-07-04 15:55:22 +0200

    added github links, expanded about page and added image

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from 'svelte';
   import axios from 'axios';
+  import devImg from '$lib/pics/Pic1.png';
 
   let exampleData = '';
 
@@ -18,15 +19,27 @@
   <title>About</title>
   <meta name="description" content="About this project" />
 </svelte:head>
+<div class="flex flex-col justify-evenly space-y-4">
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <h2 class="text-2xl font-bold mb-2">About this project</h2>
+    <p class="text-gray-700 mb-4">This is an application built for the 2024 Hacking By The Sea Hackathon.</p>
+    <p class="text-gray-500">Built by Team 16 - <a href="https://github.com/Vanishonko">Ivan</a>, <a href="https://github.com/mvv7777">Mikhail</a>, <a href="https://github.com/g0sh06">Simeon</a> and <a href="https://github.com/Silvanbr">Silvan</a>.</p>
+    <p class="text-gray-500">The project is a dashboard that provides insight into the availability and workload of both teachers and students.</p>
+    <p class="text-gray-500">It is built using <a href="https://github.com/Skibidislices/frontEnd">SvelteKit</a> and <a href="https://github.com/Skibidislices/backEnd">NodeJS (ExpressJS)</a>.</p>
+  </div>
 
-<div class="bg-white p-6 rounded-lg shadow-md">
-  <h2 class="text-2xl font-bold mb-2">About this project</h2>
-  <p class="text-gray-700 mb-4">This is an application built for the 2024 Hacking By The Sea Hackathon.</p>
-  <p class="text-gray-500">It's built by Team 16 - Ivan, Mikhail, Simeon and Silvan.</p>
-  <p class="text-gray-500">The project is a dashboard that provides insight into the availability and workload of both teachers and students.</p>
-  <p class="text-gray-500">It is built using SvelteKit and NodeJS (ExpressJS).</p>
+
+  <div class="bg-white p-6 rounded-lg shadow-md">
+      <h2 class="text-2xl font-bold mb-2">Dev Process</h2>
+      <img src={devImg} alt="Dev Process" class="object-scale-down h-48 w-64 rounded-lg shadow-md mb-4">
+  </div>
+
+
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <h2 class="text-2xl font-bold mb-2">Learning Outcomes</h2>
+    <p class="text-gray-700 mb-4">TODO</p>
+  </div>
 </div>
-
 <style>
   /* Please use tailwindcss predefined classes as much as possible */
 </style>
